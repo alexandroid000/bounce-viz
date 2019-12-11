c3fd4680f0bf0ab1ada86a66b3ca1ed17e755f37
Alli Nilles <anilles137@gmail.com>
2019-12-11 15:28:32 -0600

    remove stability script

--- src/stability_viz.py
+++
@@ -1,8 +0,0 @@
-# visualize:
-
-# for given theta, dP x dP and label as stable or unstable (color code regions
-# of stability)
-
-# highlight sections of boundary that are contracting for a specific angle theta
-# slide theta and watch sections change - when do critical events happen? How
-# many angles do we need to "cover" the polygon?
